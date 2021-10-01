878cb46503896b2537c5132e251abe5b745c0d75
Max <v.mxvlasov@gmail.com>
2021-10-02 02:29:32 +0300

    Update script.js
    Changed to working version

--- script.js
+++ script.js
@@ -1,33 +1,94 @@
-this.pay = function () {
+var who_help= 'Пожертвование';
+var email =  "";
+var price_sel = 50;
+var podp = 'Подписка.';
+var number_cloud =1;
+
+this.paySample = function () {
     var widget = new cp.CloudPayments();
+
+
+    var receipt = {
+        'Items': [
+            {
+                'label': 'Товар 1',
+                'price': parseFloat($('#sum-fld').val()),
+                'quantity': 1,
+                'amount': parseFloat($('#sum-fld').val()),
+                'vat': 0,
+                'measurementUnit': 'шт',
+                'PurveyorData': {
+                    'Phone': '+79250002233'
+                }
+            }
+        ],
+        'email': 'm.vlasov@cloudpayments.ru',
+        'phone': '89092223344'
+    };
+
     var data = {
-        name: $('#name-fld').val(),
-        lastName: $('#lastname-fld').val(),
-        phone: $('#phone-fld').val()
+        "name": "Sasha",
+        "lastName": "Grey",
+        "phone": $('#phone-fld').val(),
+        "myProp": "myVal",
+        "cloudPayments": {
+            "customerReceipt": receipt,
+        }
+
     };
 
     var amount = parseFloat($('#sum-fld').val());
     var accountId = $('#email-fld').val();
+    var currency = $('#currency-fld').val();
+    var email = '';
+    
 
-    widget.pay('charge',
-	{ // options
-            //publicId: 'pk_bc1607517b817e2be8555d2a5d229',  //id из личного кабинета
-            publicId: 'test_api_00000000000000000000001',
-            description: 'Помощь фонду \'Дети в лете\'', //назначение
-            amount: amount, //сумма
-            currency: 'RUB', //валюта
-            accountId: accountId, //идентификатор плательщика (необязательно)
-            email: accountId,
-            data: data
+    var subscribe = $('#subscr-fld').is(':checked'); //проверка
+
+    if (subscribe) { //включаем подписку
+        data.cloudPayments = {
+            recurrent: {
+                interval: 'Month',
+                period: 1
+            } //один раз в месяц начиная со следующего месяца
+        }
+    }
+// if ($('#currency-fld').val == 'RUB')
+// {$description = 'Пожертвование в фонд \'Дети в лете\'}
+
+//  else
+//  {$description:'Оплата на Тестовый 21'}
+
+    widget.charge({ // options
+            'publicId': (currency == 'RUB') ? 'pk_bc1607517b817e2be8555d2a5d229' : 'pk_a89d43ece867a72c69b976c4f2451', //id из личного кабинета 
+            'description': (currency == 'RUB') ? 'Пожертвование в фонд \'Дети в лете\'' : 'Оплата на Тестовый 21',
+            'amount': parseFloat($('#sum-fld').val()), //сумма
+            'currency': currency, //валюта
+            'invoiceId': number_cloud,
+            'email': accountId,
+            'accountId': accountId,//accountId, //идентификатор плательщика (обязательно для создания подписки)
+            'skin': 'mini',
+            'data': data
         },
-        {
-	    onSuccess: function (options) { // success
-            //действие при успешной оплате
-            },
-	    onFail: function (reason, options) { // fail
-            //действие при неуспешной оплате
-            }
-	}
-    );
-}; 
-$('#checkout-btn').click(pay);   
+        function() { //success
+            //$('.checkout-result').text('Оплата успешно завершена');
+            number_cloud++;
+        },
+        function () { // fail
+            $('.checkout-result').text('Оплата не удалась'); //действие при неуспешной оплате
+            
+        });
+    $('.btn btn-def').hide();
+};
+
+$('#checkout-btn').click(function (event) {
+    event.preventDefault();
+    paySample();
+});
+
+// jQuery(document).on('click', function(event){
+//         event.preventDefault();
+//       email = $('.email-fld').val();
+//       price_sel =  $('.sum-fld').val();
+//         paySample();
+// });
